0889bc3e23855c54e3b2faba52f8614a8d710633
anijor <anijorshrestha@gmail.com>
2017-01-01 22:34:11 +0545

    insert

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,6 +18,16 @@ public class Main {
             inputOne = input.nextLine().toLowerCase();
             divider();
 
+            try {
+                if (checkCreateTableQuery(inputOne) || checkInsertIntoQuery(inputOne))
+                    System.out.println("Query is correct");
+                else
+                    System.out.println("Query is incorrect");
+            }catch(Exception e){
+
+                System.out.println("Query is incorrect");
+            }
+
 
         }   while(false);
 //        }   while(choiceOwn != "quit");
@@ -54,5 +64,121 @@ public class Main {
 
         }
         return answer;
+    }
+    public static boolean checkCreateTableQuery(String query) throws Exception{
+
+        if(!query.contains("create"))
+            return false;
+
+        String queryFirstPart = query.substring(0, query.indexOf('(')).toLowerCase();
+        String queryVariablesPart = query.substring(query.indexOf('(') + 1, query.lastIndexOf(')')).toLowerCase();
+
+        if(queryFirstPart.matches("^create table \\w+")){
+
+//            for variables part
+            if(queryVariablesPart.contains(",")){
+
+                String variables[] = queryVariablesPart.split(",");
+
+                for(String variable: variables){
+
+                    if(!variable.trim().matches("\\w+ ((?:varchar)\\(\\d+\\)|int)"))
+                        return false;
+                }
+            }else{
+
+                if(!queryVariablesPart.trim().matches("\\w+ ((?:varchar)\\(\\d+\\)|int)"))
+                    return false;
+            }
+
+            return true;
+        }
+
+        return false;
+        //tests
+        //create table tableName(v1 int, v2 varchar(100));
+        //create table tableName(v1 int not null v2 varchar(1));
+        //create table tableName(v1 int not null, v2 varchar());
+        //create table tableName(v1 int not null, v2 varchar);
+        //create table tableName(v1 v2 varchar);
+    }
+
+    public static boolean checkInsertIntoQuery(String query) throws Exception{
+
+        if(!query.contains("insert"))
+            return false;
+
+        String queryFirstPart = query.substring(0, query.indexOf('(')).toLowerCase();
+//        System.out.println(queryFirstPart);
+
+        if(queryFirstPart.matches("^insert into \\w+ values")){
+            String queryValuesPart = query.substring(query.indexOf('(') + 1, query.lastIndexOf(')'));
+            System.out.println(queryValuesPart);
+
+//          for variables part
+            if(queryValuesPart.contains(",")){
+
+                String values[] = queryValuesPart.split(",");
+
+                for(String value: values){
+
+                    if(!value.trim().matches("\\d+|\\'\\w*\\'"))
+                        return false;
+                }
+            }else{
+
+                if(!queryValuesPart.trim().matches("\\d+|\\'\\w*\\'"))
+                    return false;
+            }
+
+            return true;
+        }else if(queryFirstPart.matches("^insert into \\w+")){
+
+            String queryVariablesPart = query.substring(query.indexOf('(') + 1, query.indexOf(" values") - 1);
+            String queryValuesPart = query.substring(query.indexOf("values") + 7, query.lastIndexOf(')'));
+            System.out.println(queryVariablesPart);
+            System.out.println(queryValuesPart);
+
+//          for variables part
+            if(queryVariablesPart.contains(",") && queryValuesPart.contains(",")){
+
+                String variables[] = queryVariablesPart.split(",");
+                String values[] = queryValuesPart.split(",");
+
+                if(variables.length != values.length)
+                    return false;
+
+                for(String value: values){
+
+                    if(!value.trim().matches("\\d+|\\'\\w*\\'"))
+                        return false;
+                }
+            }else{
+
+                if(!queryValuesPart.trim().matches("\\d+|\\'\\w*\\'"))
+                    return false;
+            }
+
+            return true;
+        }else {
+
+            if (!queryFirstPart.contains("into"))
+                System.err.println("into is missing");
+
+            if (!queryFirstPart.contains("values"))
+                System.err.println("values is missing");
+        }
+
+        return false;
+        //tests
+        //insert into tableName() values();
+        //insert into tableName(v1) values('');
+        //insert into tableName values('');
+        //insert into tableName(v1, v2, v3, v4) values(1, 2, 3, 4);
+        //insert into tableName(v1, v2, v3, v4) values(1, 'asdfasdf', 3, 'asdfasdf');
+        //insert into tableName values(1, 'asdfasf', 3, 'asasdfasdf')
+        //insert into tableName values('asasdfasdf')
+        //insert into tableName values(1, 'asdfasf', 3, 'asasdfasdf'
+        //insert into tableName values(3)
     }
 }
